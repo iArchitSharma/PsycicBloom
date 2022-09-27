a2b56a5ca1a1d9f07576bfce8963e6d6e2b5d294
Archit Sharma <archit8679@gmail.com>
2022-09-27 21:56:21 +0530

    config edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,16 @@ module.exports = {
     extend: {
       colors: {
         myCustom: {
-          bg: '#fffffe',  //Background color
-          headline: '#094067',
-          pg: '#5f6c7b',
-          btn: '#3da9fc',
-          text: '#fffffe',
-          500: '#bfa094',
-          600: '#a18072',
-          700: '#977669',
-          800: '#846358',
-          900: '#43302b',
+          'bg': '#fffffe',  //Background color
+          'headline': '#094067',
+          'pg': '#5f6c7b',
+          'btn': '#3da9fc',
+          'text': '#fffffe',
+          '500': '#bfa094',
+          '600': '#a18072',
+          '700': '#977669',
+          '800': '#846358',
+          '900': '#43302b',
         },
       },
       fontFamily: {
