d9f8d697183ad136cc8315448ae6c7e1280b34a7
Archit Sharma <archit8679@gmail.com>
2022-09-27 16:30:38 +0530

    html structure update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
   "./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myCustom: {
+          bg: '#fffffe',  //Background color
+          headline: '#094067',
+          pg: '#5f6c7b',
+          btn: '#3da9fc',
+          text: '#fffffe',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      },
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
